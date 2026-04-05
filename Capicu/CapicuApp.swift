fd13c71b161a27de740d86f8c1ddb6d8eb6b8832
andrespesquera1 <andres@pesquera.com>
2026-04-05 11:26:23 -0500

    Initial Commit

---
+++ Capicu/CapicuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapicuApp.swift
+//  Capicu
+//
+//  Created by Andrés Pesquera on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct CapicuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
